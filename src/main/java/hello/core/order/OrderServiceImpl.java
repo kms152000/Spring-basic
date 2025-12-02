6a67c799298e59c7ef7999e14f7442ac8d435942
kms152000 <kms152000@naver.com>
2025-12-02 12:34:29 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -24,4 +24,7 @@ public class OrderServiceImpl implements OrderService {
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
